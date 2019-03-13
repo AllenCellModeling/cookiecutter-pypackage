f60149d7a23dfe16892463f65c922dc5a2dfecb7
JacksonMaxfield <jacksonb@alleninstitute.org>
2019-03-13 11:18:49 -0700

    Add extra sphinx extensions

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -32,7 +32,17 @@ import {{ cookiecutter.project_slug }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+# Control napoleon
+napoleon_google_docstring = False
+napolean_include_init_with_doc = True
+napoleon_use_ivar = True
+napoleon_use_param = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
